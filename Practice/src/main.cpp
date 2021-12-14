dfd8aeeca81a46c19188072ddce5943970ae8807
zmocheng <zmocheng@gmail.com>
2021-12-14 17:52:16 +0100

    Add orient() fucntion to judge the direction of three points: CW or CCW.

--- Practice/src/main.cpp
+++ Practice/src/main.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 #include<vector>
+#include<limits>
 
 // https://en.wikipedia.org/wiki/Kahan_summation_algorithm
 // https://github.com/delfrrr/delaunator-cpp/tree/master/include
+
 double sum(const std::vector<double>& x) {
     double sum = x[0];
     double err = 0.0;
@@ -17,6 +19,19 @@ double sum(const std::vector<double>& x) {
 }
 
 
+// orient:
+// @brief: if CW return 0 else(CCW) return 1
+bool orient(
+    const double px,
+    const double py,
+    const double qx,
+    const double qy,
+    const double rx,
+    const double ry) {
+    return (qy - py) * (rx - qx) - (qx - px) * (ry - qy) < 0.0;
+}
+
+
 int main(int argc, const char** argv) {
 
     std::ios::sync_with_stdio(false); // speed up for cin and std::std::cout
@@ -25,7 +40,9 @@ int main(int argc, const char** argv) {
     vec.emplace_back(1.0);
     vec.emplace_back(-2.0);
     vec.emplace_back(3.0);
-    std::cout << sum(vec);
+    std::cout << sum(vec) << '\n';
+    //std::cout << std::numeric_limits<double>::max;
 
+    std::cout << orient(2, 0, 1, 1, 0, 0);
 	return 0;
 }
