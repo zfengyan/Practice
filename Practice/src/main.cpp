b1c40b80562dc6b1145dfb6374cb58f84d009eec
zmocheng <zmocheng@gmail.com>
2022-01-11 23:35:38 +0100

    Organize notes.

--- Practice/src/main.cpp
+++ Practice/src/main.cpp
@@ -110,46 +110,6 @@ int main(int argc, const char** argv) {
     }
 
     glfwTerminate();
-
-
-    //std::ios::sync_with_stdio(false); // speed up for cin and std::std::cout
-    //std::vector<double> vec;
-    //vec.emplace_back(1.0);
-    //vec.emplace_back(-2.0);
-    //vec.emplace_back(3.0);
-    ////std::cout << sum(vec) << '\n';
-    ////std::cout << std::numeric_limits<double>::max;
-
-    ////std::cout << orient(2, 0, 1, 1, 0, 0) << '\n';
-    ////std::cout << circumcenter(0, 0, 1, 1, 2, 0).first << ' ' << circumcenter(0, 0, 1, 1, 2, 0).second << '\n';
-
-    //Eigen::VectorXd test_eigen(3);
-    //test_eigen(0) = 1;
-    //test_eigen(1) = 2;
-    //test_eigen(2) = 3;
-    ////std::cout << test_eigen;
-
-    //geo::Vector3d vec1(1, 1, 1);
-    //geo::Vector3d vec2(2, 2, 2);
-    //vec1 += vec2;
-    ////std::cout << vec1.v[0] << '\n';
-    ////std::cout << vec1.vector_length();
-
-    //geo::Vector3d vec3(vec1+vec2);
-    //std::cout << vec3.normalized_vector().v[0] << '\n';
-
-    //geo::Vector3d vec_d(vec3 / 0);
-    //std::cout << vec_d.v[0] << '\n';
-
-    //geo::Plane plane(1,1,1,0);
-    //std::cout << plane.is_plane() << '\n';
-    //geo::Point p(1, 1, 2);
-    //std::cout << plane.point_in_plane(p)<<'\n';
-
-    //geo::Triangle triangle;
-    //std::cout << triangle.pa.x << '\n';
-
-    //std::cout << "verlet: " << verlet(5, -10, 0.01) << '\n';
     
     return 0;
 }
