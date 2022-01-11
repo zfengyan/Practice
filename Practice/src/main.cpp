ef1318fa3059e564e9818a2f96a9428472470284
zmocheng <zmocheng@gmail.com>
2022-01-11 23:32:25 +0100

    Add aditional files and dependencies for OpenGL.

--- Practice/src/main.cpp
+++ Practice/src/main.cpp
@@ -5,6 +5,8 @@
 #include<Eigen/Core>
 #include"Geometry.h"
 
+#include<GLFW/glfw3.h>
+
 // https://en.wikipedia.org/wiki/Kahan_summation_algorithm
 // https://github.com/delfrrr/delaunator-cpp/tree/master/include
 // @brief: calculate the sum of all items in a vector
@@ -77,46 +79,77 @@ double verlet(double pos, double acc, double dt) {
 
 int main(int argc, const char** argv) {
 
-    std::ios::sync_with_stdio(false); // speed up for cin and std::std::cout
+    GLFWwindow* window;
 
-    std::vector<double> vec;
-    vec.emplace_back(1.0);
-    vec.emplace_back(-2.0);
-    vec.emplace_back(3.0);
-    //std::cout << sum(vec) << '\n';
-    //std::cout << std::numeric_limits<double>::max;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
 
-    //std::cout << orient(2, 0, 1, 1, 0, 0) << '\n';
-    //std::cout << circumcenter(0, 0, 1, 1, 2, 0).first << ' ' << circumcenter(0, 0, 1, 1, 2, 0).second << '\n';
-	
-    Eigen::VectorXd test_eigen(3);
-    test_eigen(0) = 1;
-    test_eigen(1) = 2;
-    test_eigen(2) = 3;
-    //std::cout << test_eigen;
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
 
-    geo::Vector3d vec1(1, 1, 1);
-    geo::Vector3d vec2(2, 2, 2);
-    vec1 += vec2;
-    //std::cout << vec1.v[0] << '\n';
-    //std::cout << vec1.vector_length();
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
 
-    geo::Vector3d vec3(vec1+vec2);
-    std::cout << vec3.normalized_vector().v[0] << '\n';
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
 
-    geo::Vector3d vec_d(vec3 / 0);
-    std::cout << vec_d.v[0] << '\n';
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
 
-    geo::Plane plane(1,1,1,0);
-    std::cout << plane.is_plane() << '\n';
-    geo::Point p(1, 1, 2);
-    std::cout << plane.point_in_plane(p)<<'\n';
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
 
-    geo::Triangle triangle;
-    std::cout << triangle.pa.x << '\n';
+    glfwTerminate();
 
-    std::cout << "verlet: " << verlet(5, -10, 0.01) << '\n';
 
+    //std::ios::sync_with_stdio(false); // speed up for cin and std::std::cout
+    //std::vector<double> vec;
+    //vec.emplace_back(1.0);
+    //vec.emplace_back(-2.0);
+    //vec.emplace_back(3.0);
+    ////std::cout << sum(vec) << '\n';
+    ////std::cout << std::numeric_limits<double>::max;
+
+    ////std::cout << orient(2, 0, 1, 1, 0, 0) << '\n';
+    ////std::cout << circumcenter(0, 0, 1, 1, 2, 0).first << ' ' << circumcenter(0, 0, 1, 1, 2, 0).second << '\n';
+
+    //Eigen::VectorXd test_eigen(3);
+    //test_eigen(0) = 1;
+    //test_eigen(1) = 2;
+    //test_eigen(2) = 3;
+    ////std::cout << test_eigen;
+
+    //geo::Vector3d vec1(1, 1, 1);
+    //geo::Vector3d vec2(2, 2, 2);
+    //vec1 += vec2;
+    ////std::cout << vec1.v[0] << '\n';
+    ////std::cout << vec1.vector_length();
+
+    //geo::Vector3d vec3(vec1+vec2);
+    //std::cout << vec3.normalized_vector().v[0] << '\n';
+
+    //geo::Vector3d vec_d(vec3 / 0);
+    //std::cout << vec_d.v[0] << '\n';
+
+    //geo::Plane plane(1,1,1,0);
+    //std::cout << plane.is_plane() << '\n';
+    //geo::Point p(1, 1, 2);
+    //std::cout << plane.point_in_plane(p)<<'\n';
+
+    //geo::Triangle triangle;
+    //std::cout << triangle.pa.x << '\n';
+
+    //std::cout << "verlet: " << verlet(5, -10, 0.01) << '\n';
     
     return 0;
 }
